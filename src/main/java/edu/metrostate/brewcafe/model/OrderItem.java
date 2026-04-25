d507ceab2936bf13b80a0cd539242be24341f31f
mrcheevang <152324160+mrcheevang@users.noreply.github.com>
2026-04-25 00:00:06 -0500

    Changed OrderItem with size and customization options

--- src/main/java/edu/metrostate/brewcafe/model/OrderItem.java
+++ src/main/java/edu/metrostate/brewcafe/model/OrderItem.java
@@ -1,12 +1,27 @@
 package edu.metrostate.brewcafe.model;
 
-public class OrderItem {
-    private final MenuItem menuItem;
-    private int quantity;
+import java.util.ArrayList;
+import java.util.List;
 
-    public OrderItem(MenuItem menuItem, int quantity) {
+public class OrderItem {
+	private final MenuItem menuItem;
+    private int quantity;
+    private SizeOption selectedSize;
+    private List<Customization> selectedCustomizations;
+
+    public OrderItem(MenuItem menuItem, int quantity,
+    		SizeOption selectedSize, List<Customization> selectedCustomization) {
         this.menuItem = menuItem;
         this.quantity = quantity;
+        this.selectedSize = selectedSize;
+        this.selectedCustomizations = new ArrayList<>();
+        
+        if (selectedCustomization != null)
+        	this.selectedCustomizations.addAll(selectedCustomization);
+    }
+    
+    public OrderItem(MenuItem menuItem, int quantity) {
+    	this(menuItem, quantity, null, new ArrayList<>());
     }
 
     public MenuItem getMenuItem() {
@@ -19,5 +34,23 @@ public class OrderItem {
 
     public void setQuantity(int quantity) {
         this.quantity = quantity;
+    }
+    
+    public SizeOption getSelectedSize() {
+    	return selectedSize;
+    }
+    
+    public void setSelectedSize(SizeOption selectedSize) {
+    	this.selectedSize = selectedSize;
+    }
+    
+    public List<Customization> getSelectedCustomizations() {
+    	return selectedCustomizations;
+    }
+    
+    public void setSelectedCustomizations(List<Customization> newCustomizations) {
+    	selectedCustomizations.clear();
+    	if (newCustomizations != null)
+    		selectedCustomizations.addAll(newCustomizations);
     }
 }
